ceab545f079d0e46825e8dedb92d788112e6dda6
Alexandre Menezes <amenezes031@gmail.com>
2021-11-14 16:56:24 -0300

    simplify CacheStore insertion with a replace method

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -3,4 +3,5 @@ import { SavePurchases } from "@/domain/usecases";
 export interface CacheStore {
   delete: (key: string) => void;
   insert: (key: string, value: any) => void;
+  replace: (key: string, value: any) => void;
 }
